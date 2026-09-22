57858520b0116bf571e3d9e7d69ecabad2eb21ed
WeiminKng <weiminkong2@gmail.com>
2026-09-22 22:41:45 +0800

    使用nix托管

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Development environment for kilo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          clang
+          gnumake
+          git
+        ];
+      };
+    };
+}
